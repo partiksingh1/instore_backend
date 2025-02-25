9371a294b031c26d8743908aa5f99e050190d65f
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-02-25 22:46:28 +0530

    fix 1

--- src/types/video.types.ts
+++ src/types/video.types.ts
@@ -1,15 +1,15 @@
-import { Request } from 'express';
+// import { Request } from 'express';
 
-export interface ProcessedVideo {
-  outputUrl: string;
-}
+// export interface ProcessedVideo {
+//   outputUrl: string;
+// }
 
-export interface VideoRequest extends Request {
-  files: {
-    [fieldname: string]: Express.Multer.File[];
-  };
-}
+// export interface VideoRequest extends Request {
+//   files: {
+//     [fieldname: string]: Express.Multer.File[];
+//   };
+// }
 
-export interface VideoProcessingError extends Error {
-  statusCode?: number;
-}
+// export interface VideoProcessingError extends Error {
+//   statusCode?: number;
+// }
